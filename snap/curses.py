f9eac6749181b1810713c4d44b12cccf43b38037
nerp <nerp@rhea>
2020-11-24 14:16:24 -0500

    Adding Help Screen

--- snap/curses.py
+++ snap/curses.py
@@ -1,22 +1,9 @@
 import curses
 from snap import status_string
-from snap import snap
+from snap.state import State
 import logging
 
 _LOGGER = logging.getLogger(__name__)
-
-def draw_clients(stdscr, y_offset, clients, selected):
-    for idx, client in enumerate(clients):
-        #_LOGGER.info("%s :: %s", type(client), client.muted)
-        if selected == client:
-            color = curses.color_pair(1)
-        elif client.muted:
-            color = curses.color_pair(2)
-        else:
-            color = curses.color_pair(4)
-
-        client_display = status_string(client)
-        stdscr.addstr(y_offset + idx, 0, client_display, color)
 
 def initColors():
     curses.start_color()
@@ -39,48 +26,67 @@ def handle_keypress(key, state):
     if key == ord('s'):
         state.next_stream()
 
-class State():
-    """ Representation of the application state. Methods for modifying state.  """
-    def __init__(self):
-        self.clients = snap.server.clients
-        self.client = self.clients[0]
-        self.active_stream = snap.get_active_stream()
-        self.streams = snap.server.streams
+class MainScreen():
+    def __init__(self, stdscr):
+        self.stdscr = stdscr
 
-    def next_stream(self):
-        if(self.active_stream):
-            idx = self.streams.index(self.active_stream) + 1
-            if idx >= len(self.streams):
-                idx = 0
-            stream = self.streams[idx]
-            snap.set_stream(stream)
-            self.active_stream = stream
+    def draw(self, state):
+        out = "Streams: "
+        for stream in state.streams:
+            if stream == state.active_stream:
+                out += "[ {} ] ".format(stream.name)
+            else:
+                out += "{} ".format(stream.name)
+        self.stdscr.addstr(0, 0, out)
 
-    def next_client(self):
-        y = self.clients.index(self.client)
-        y = y + 1
-        y = min(len(self.clients)-1, y)
-        self.client = self.clients[y]
+        for idx, client in enumerate(state.clients):
+        #_LOGGER.info("%s :: %s", type(client), client.muted)
+            if state.client == client:
+                color = curses.color_pair(1)
+            elif client.muted:
+                color = curses.color_pair(2)
+            else:
+                color = curses.color_pair(4)
 
-    def prev_client(self):
-        y = self.clients.index(self.client)
-        y = y - 1
-        y = max(0, y)
-        self.client = self.clients[y]
+            client_display = status_string(client)
+            self.stdscr.addstr(2 + idx, 0, client_display, color)
 
-    def toggle_mute(self):
-       if(self.client.muted):
-           snap.mute(self.client, False)
-       else:
-           snap.mute(self.client, True)
 
-    def lower_volume(self):
-       volume = max(0, self.client.volume - 5)
-       snap.set_volume(self.client, volume)
+    def draw_status_bar(self):
+        height, width = self.stdscr.getmaxyx()
+        statusbarstr = "Press 'q' to exit , '1' for help, 'hjkl' for volume , or 's' to change stream"
+        self.stdscr.attron(curses.color_pair(3))
+        self.stdscr.addstr(height-1, 0, statusbarstr)
+        self.stdscr.addstr(height-1, len(statusbarstr), " " * (width - len(statusbarstr) - 1))
+        self.stdscr.attroff(curses.color_pair(3))
 
-    def raise_volume(self):
-       volume = min(100, self.client.volume + 5)
-       snap.set_volume(self.client, volume)
+class HelpScreen():
+    def __init__(self, stdscr):
+        self.stdscr = stdscr
+    def draw(self, state):
+        self.stdscr.addstr(0, 0, "Navigation")
+        self.stdscr.addstr(1, 0, "-------")
+        self.stdscr.addstr(2, 0, "       ")
+        self.stdscr.addstr(3, 0, "1     Help Screen (this screen)")
+        self.stdscr.addstr(4, 0, "2     Main Screen")
+        self.stdscr.addstr(5, 0, "3     Client Screen")
+        self.stdscr.addstr(6, 0, "q     quit application")
+        self.stdscr.addstr(7, 0, "       ")
+        self.stdscr.addstr(8, 0, "Main Screen")
+        self.stdscr.addstr(9, 0, "-----------")
+        self.stdscr.addstr(10, 0, "           ")
+        self.stdscr.addstr(11, 0, "j,k   change active client")
+        self.stdscr.addstr(12, 0, "s     change active stream")
+        self.stdscr.addstr(13, 0, "h     lower volume on selected client")
+        self.stdscr.addstr(14, 0, "l     raise volume on selected client")
+
+    def draw_status_bar(self):
+        height, width = self.stdscr.getmaxyx()
+        statusbarstr = "Press 'q' to exit "
+        self.stdscr.attron(curses.color_pair(3))
+        self.stdscr.addstr(height-1, 0, statusbarstr)
+        self.stdscr.addstr(height-1, len(statusbarstr), " " * (width - len(statusbarstr) - 1))
+        self.stdscr.attroff(curses.color_pair(3))
 
 def draw_screen(stdscr):
     _LOGGER.info("Starting ncsnpcc")
@@ -91,35 +97,23 @@ def draw_screen(stdscr):
     key = 0
 
     state = State()
+    screen = MainScreen(stdscr)
 
     while (key != ord('q')):
         stdscr.clear()
 
         handle_keypress(key, state)
-        # Draw Screen
-        draw_streams(stdscr, 0, state.streams, state.active_stream)
-        draw_clients(stdscr, 2, state.clients, state.client)
-        draw_status_bar(stdscr)
+
+        if key == ord('1'):
+            screen = HelpScreen(stdscr)
+        if key == ord('2'):
+            screen = MainScreen(stdscr)
+
+        screen.draw(state)
+        screen.draw_status_bar()
         stdscr.refresh()
 
         key = stdscr.getch()
-
-def draw_streams(stdscr, y_offset, streams, active_stream):
-    out = "Streams: "
-    for stream in streams:
-        if stream == active_stream:
-            out += "[ {} ] ".format(active_stream.name)
-        else:
-            out += "{} ".format(stream.name)
-    stdscr.addstr(y_offset, 0, out)
-
-def draw_status_bar(stdscr):
-    height, width = stdscr.getmaxyx()
-    statusbarstr = "Press 'q' to exit , 'hjkl' for volume , or 's' to change stream"
-    stdscr.attron(curses.color_pair(3))
-    stdscr.addstr(height-1, 0, statusbarstr)
-    stdscr.addstr(height-1, len(statusbarstr), " " * (width - len(statusbarstr) - 1))
-    stdscr.attroff(curses.color_pair(3))
 
 def main():
     curses.wrapper(draw_screen)
---
+++ snap/state.py
@@ -0,0 +1,50 @@
+
+from snap import snap
+import logging
+
+_LOGGER = logging.getLogger(__name__)
+
+class State():
+    """ Representation of the application state. Methods for modifying state.  """
+    def __init__(self):
+        self.clients = snap.server.clients
+        self.client = self.clients[0]
+        self.active_stream = snap.get_active_stream()
+        self.streams = snap.server.streams
+
+    def next_stream(self):
+        if(self.active_stream):
+            idx = self.streams.index(self.active_stream) + 1
+            if idx >= len(self.streams):
+                idx = 0
+            stream = self.streams[idx]
+            snap.set_stream(stream)
+            self.active_stream = stream
+
+    def next_client(self):
+        y = self.clients.index(self.client)
+        y = y + 1
+        y = min(len(self.clients)-1, y)
+        self.client = self.clients[y]
+
+    def prev_client(self):
+        y = self.clients.index(self.client)
+        y = y - 1
+        y = max(0, y)
+        self.client = self.clients[y]
+
+    def toggle_mute(self):
+       if(self.client.muted):
+           snap.mute(self.client, False)
+       else:
+           snap.mute(self.client, True)
+
+    def lower_volume(self):
+       volume = max(0, self.client.volume - 5)
+       snap.set_volume(self.client, volume)
+
+    def raise_volume(self):
+       volume = min(100, self.client.volume + 5)
+       snap.set_volume(self.client, volume)
+
+
